b35e7a4a58f219dcc23950320771fbc535aae065
Illo Yoon <yildbs@danusys.com>
2018-09-04 17:43:23 +0900

    * File saving implemented

--- DanusysVotingBot.py
+++ DanusysVotingBot.py
@@ -2,7 +2,12 @@
  
 import os
 from flask import Flask, request, jsonify
- 
+import glob
+
+try:
+    os.mkdir('data') 
+except:
+    pass
 app = Flask(__name__)
  
 @app.route('/keyboard')
@@ -14,15 +19,36 @@ def Keyboard():
     return jsonify(dataSend)
 
 class User:
-    def __init__(self, user_key, name):
+    def __init__(self, user_key):
         self.user_key = user_key
-        self.name = name
-        self.state = 'DEFAULT'
+        self.name = ''
         self.voteto = ''
+        self.state = 'DEFAULT'
+        self.adminstate = 'DEFAULT'
+        self.modified = False
+        
+    def save(self):
+        if self.modified:
+            with open('data/'+self.user_key, 'w', encoding='UTF8') as f:
+                f.write(self.user_key+','+self.name+','+self.state+','+self.adminstate+','+self.voteto)
+            self.modified = False
 
 class Users:
     def __init__(self):
         self.users = {}
+    
+    def save(self, user_key):
+        self.users[user_key].save()
+    
+    def loadfromdata(self):
+        for filename in glob.glob('data/*'):
+            with open(filename, 'r', encoding='UTF8') as f:
+                line = f.read()
+                line = line.replace('\n', '')
+                user_key, name, state, adminstate, voteto = line.split(',')
+                self.adduser(user_key)
+                self.setusername(user_key, name)
+                self.setuservoteto(user_key, voteto)
 
     def find(self, user_key):
         for _, user in self.users.items():
@@ -36,9 +62,14 @@ class Users:
                 return True
         return False
 
-    def adduser(self, user_key, name):
-        self.users[user_key] = User(user_key, name)
+    def adduser(self, user_key):
+        self.users[user_key] = User(user_key)
+        self.users[user_key].modified = True
 
+    def setusername(self, user_key, name):
+        self.users[user_key].name = name
+        self.users[user_key].modified = True
+        
     def getusername(self, user_key):
         try:
             return self.users[user_key].name
@@ -54,11 +85,26 @@ class Users:
     def setuserstate(self, user_key, state):
         try:
             self.users[user_key].state = state
+            self.users[user_key].modified = True
         except:
             return 'NOEXISTINGUSER'
 
+    def getuseradminstate(self, user_key):
+        try:
+            return self.users[user_key].adminstate
+        except:
+            return 'NOEXISTINGUSER'
+            
+    def setuseradminstate(self, user_key, adminstate):
+        try:
+            self.users[user_key].adminstate = adminstate
+            self.users[user_key].modified = True
+        except:
+            return 'NOEXISTINGUSER'        
+    
     def setuservoteto(self, user_key, excellent):
         self.users[user_key].voteto = excellent
+        self.users[user_key].modified = True
         
     def getuservoteto(self, user_key):
         return self.users[user_key].voteto
@@ -78,7 +124,7 @@ class Users:
         text2 = ""
         for voteto, num in counting.items():
             text2 += voteto + ' - ' + str(num) + '표'
-        return text2 + "\n" + text + "\n"
+        return text2 + "\n\n" + text + "\n"
 
 
 REASON = "\n\n"
@@ -125,10 +171,18 @@ with open('__Password.txt', 'r', encoding='UTF8') as f:
 #print(PASSWORD)
 
 HELP = ""
-HELP += "\n\n\n다누시스투표봇사용방법\n\n"
-HELP += "'우수사원' : 우수사원 투표 시 입력\n"
+HELP += "\n다누시스투표봇사용방법\n\n"
+HELP += "'우수사원' : 입력 시 우수사원 투표\n"
+HELP += "'관리자' : 관리자 명령어 확인\n"
+
+ADMINHELP = ""
+ADMINHELP += "\n다누시스투표봇 관리자 명령어\n\n"
+ADMINHELP += "'관리자' : 관리자 명령어 확인\n"
+ADMINHELP += "'투표결과' : 투표 결과 확인\n"
+#ADMINHELP += "'결과제거' : 투표 결과 제거\n"
 
 USERS = Users()
+USERS.loadfromdata()
 
 @app.route('/message', methods=['POST'])
 def Message():
@@ -179,8 +233,17 @@ def Message():
                     "text": "투표할 우수사원의 이름을 입력해주세요." + REASON
                 }
             }
-        elif content == u"투표현황":
+        elif content == u"관리자":
             USERS.setuserstate(user_key, 'PASSWORD')
+            USERS.setuseradminstate(user_key, 'ADMINHELP')
+            dataSend = {
+                "message": {
+                    "text": "관리자만 확인 할 수 있습니다. 관리자라면 비밀번호를 입력해주세요."
+                }
+            }    
+        elif content == u"투표결과":
+            USERS.setuserstate(user_key, 'PASSWORD')
+            USERS.setuseradminstate(user_key, 'RESULT')
             dataSend = {
                 "message": {
                     "text": "관리자만 확인 할 수 있습니다. 관리자라면 비밀번호를 입력해주세요."
@@ -202,13 +265,13 @@ def Message():
         if content not in CANDIDATES:
             dataSend = {
                 "message": {
-                    "text": content + "는 우수 사원 후보 중에 없습니다. '이름'만 다시 입력해주세요"
+                    "text": content + "는 우수 사원 후보 중에 없습니다. '이름'만 다시 입력해주세요\n"
                 }
             }
         elif GROUPS[currentusername] == GROUPS[content]:
             dataSend = {
                 "message": {
-                    "text": "같은 부서 직원에게 투표 할 수 없습니다! 다시 투표해주세요"
+                    "text": "같은 부서 직원에게 투표 할 수 없습니다! 다시 투표해주세요\n"
                 }
             }
         else:
@@ -216,7 +279,7 @@ def Message():
             USERS.setuservoteto(user_key, content)
             dataSend = {
                 "message": {
-                    "text": "당신은 " + content + " 를 뽑았습니다!" + HELP
+                    "text": "당신은 " + content + " 를 뽑았습니다!\n" + HELP
                 }
             }
 
@@ -226,24 +289,32 @@ def Message():
     elif state == 'PASSWORD':
         USERS.setuserstate(user_key, 'DEFAULT')
         if content == PASSWORD:
-            summary = USERS.getvotingsummary()
-            dataSend = {
-                "message": {
-                    "text": summary
+            if USERS.getuseradminstate(user_key) == 'ADMINHELP':
+                dataSend = {
+                    "message": {
+                        "text": ADMINHELP
+                    }
+                }        
+            elif USERS.getuseradminstate(user_key) == 'RESULT':
+                summary = USERS.getvotingsummary()
+                dataSend = {
+                    "message": {
+                        "text": summary
+                    }
                 }
-            }
         else:
             dataSend = {
                 "message": {
                     "text": "비밀번호가 틀렸습니다. 처음으로 되돌아 갑니다. " + HELP
                 }
             }
-            
+        USERS.setuseradminstate(user_key, 'DEFAULT')
+        
     ###############################
     #### FSM ENROLL: 신규 가입 메시지 출력
     ###############################
     elif state == 'ENROLL':
-        USERS.adduser(user_key, '')
+        USERS.adduser(user_key)
         USERS.setuserstate(user_key, 'MYNAME')
         dataSend = {
             "message": {
@@ -268,13 +339,17 @@ def Message():
                 }
             }
         else:
-            USERS.adduser(user_key, content)
+            USERS.adduser(user_key)
+            USERS.setusername(user_key, content)
             USERS.setuserstate(user_key, 'DEFAULT')
             dataSend = {
                 "message": {
                     "text": "당신의 이름은 " + content + " 입니다!" + HELP
                 }
             }
+    
+    USERS.save(user_key)
+    
     return jsonify(dataSend)
  
 
